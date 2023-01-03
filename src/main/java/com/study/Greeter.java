9984ff526982254e5a896e0068a514176c9b050e
Sachin Jain <sachin.jain.nsit@gmail.com>
2023-01-03 12:12:50 +0530

    Difference between an annonymous class and lambda implementation

--- src/main/java/com/study/Greeter.java
+++ src/main/java/com/study/Greeter.java
@@ -7,10 +7,31 @@ public class Greeter {
     }
 
     public static void main(String[] args) {
-        new Greeter().greet(new HelloWorldGreeting());
-        Greeting hello = () -> System.out.println("hello");
+        Greeter greeter = new Greeter();
+        //explicit instance of a Class
+        Greeting helloWorldInstance = new HelloWorldGreeting();
+        greeter.greet(helloWorldInstance);
+
+        //annonymous inner class instance;
+        Greeting annonymousInstance = new Greeting() {
+            @Override
+            public void perform() {
+                System.out.println("Hello Annonymous!");
+            }
+        };
+        greeter.greet(annonymousInstance);
+        //inline creation of function
+        Greeting helloLambda = () -> System.out.println("hello lambda");
+
+        greeter.greet(helloLambda);
+
     }
 
-    //Lambdas are just the functions that exist in isolation and do not have to belong to any class
-    //Functions as values
+    //Lambdas are just the functions that exist in isolation
+    // and do not have to belong to any class
+
+    // Functions as values
+
+    // For most purposes, we can say lambdas are just similar to annonymous class but
+    // in reality they are not annonymous class
 }
